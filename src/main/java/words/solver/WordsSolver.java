a41d078773a71d29cea3a8281273427980a49a4d
sergii.tsyban <sergii.tsyban@gmail.com>
2017-08-25 16:20:35 +0200

    move solving logic into nested class

--- src/main/java/words/solver/WordsSolver.java
+++ src/main/java/words/solver/WordsSolver.java
@@ -4,65 +4,27 @@ import java.util.*;
 
 public class WordsSolver {
 
-    public static final int DEFAULT_MAX_STEPS = 10;
+    public static final int MAX_STEPS = 10;
 
     private Map<Integer, Set<String>> lengthSeparatedDictionary = new HashMap<>();
-    private int maxSteps  = DEFAULT_MAX_STEPS;
 
     public WordsSolver(String[] dictionary){
         separateByLength(dictionary);
     }
 
-    public List<String>  solve(String from, String to){
+    public List<String> solve(String from, String to){
         if(from.length() != to.length()){
             throw new IllegalArgumentException("Words should have same length");
         }
         if(from.length() < 2){
             throw new IllegalArgumentException("Words should have length >= 2");
         }
-
-        LinkedList<String> wordsStack = new LinkedList<>();
-        wordsStack.push(from);
-        boolean solved = solve(wordsStack, to, lengthSeparatedDictionary.get(from.length()));
-        return wordsStack;
+        TaskToSolve taskToSolve = new TaskToSolve(from, to, lengthSeparatedDictionary.get(from.length()));
+        taskToSolve.solve();
+        return taskToSolve.wordsStack;
     }
 
-    private boolean solve(Deque<String> wordsStack, String to, Set<String> dictionary){
-        if(wordsStack.size() >= DEFAULT_MAX_STEPS){
-            return false;
-        }
-        Set<String> nextWords = findWordsDifferByOneLetter(wordsStack.getLast(), dictionary);
-        if(nextWords.contains(to)){
-            wordsStack.add(to);
-            return true;
-        }
-        for (String nextWord : nextWords) {
-            if(wordsStack.contains(nextWord)){
-                continue;
-            }
-            wordsStack.add(nextWord);
-            boolean solved = solve(wordsStack, to, dictionary);
-            if(!solved){
-                wordsStack.removeLast();
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private Set<String> findWordsDifferByOneLetter(String last, Set<String> dictionary) {
-        Set<String> nextWords = new HashSet<>();
-        for (String w : dictionary) {
-            int diff = diffIgnoreCase(last, w);
-            if(diff == 1){
-                nextWords.add(w);
-            }
-        }
-        return nextWords;
-    }
-
-    private int diffIgnoreCase(String word, String compareTo) {
+    private static int diffIgnoreCase(String word, String compareTo) {
         int diff = 0;
         for (int i = 0; i < word.length(); i++) {
             if(word.charAt(i) != compareTo.charAt(i)){
@@ -70,13 +32,6 @@ public class WordsSolver {
             }
         }
         return diff;
-    }
-
-    public void setMaxSteps(int maxSteps) {
-        if(maxSteps < 1){
-            throw new IllegalArgumentException("Max steps cannot be less then 1");
-        }
-        this.maxSteps = maxSteps;
     }
 
     private void separateByLength(String[] dictionary) {
@@ -92,6 +47,64 @@ public class WordsSolver {
                 v.add(word);
                 return v;
             });
+        }
+    }
+
+    private static class TaskToSolve {
+
+        String from;
+        String to;
+        Set<String> dictionary;
+        LinkedList<String> wordsStack = new LinkedList<>();
+        boolean solved;
+
+        public TaskToSolve(String from, String to, Set<String> dictionary) {
+            this.from = from;
+            this.to = to;
+            this.dictionary = dictionary;
+            this.wordsStack = new LinkedList<>();
+        }
+
+        void solve(){
+            wordsStack.add(from);
+            solved = solve(wordsStack);
+            if(!solved){
+                wordsStack.clear();
+            }
+        }
+
+        boolean solve(LinkedList<String> wordsStack){
+            if(wordsStack.size() >= MAX_STEPS){
+                // maximum number of steps reached
+                return false;
+            }
+            Set<String> nextWords = findWordsDifferByOneLetter(wordsStack.getLast());
+            nextWords.removeAll(wordsStack);
+            // check we reach the goal
+            if(nextWords.contains(to)){
+                wordsStack.add(to);
+                return true;
+            }
+            for (String nextWord : nextWords) {
+                wordsStack.add(nextWord);
+                if(solve(wordsStack)){
+                    return true;
+                }
+                wordsStack.removeLast();
+            }
+            // didn't found anything
+            return false;
+        }
+
+        Set<String> findWordsDifferByOneLetter(String last) {
+            Set<String> nextWords = new HashSet<>();
+            for (String w : dictionary) {
+                int diff = diffIgnoreCase(last, w);
+                if(diff == 1){
+                    nextWords.add(w);
+                }
+            }
+            return nextWords;
         }
     }
 
