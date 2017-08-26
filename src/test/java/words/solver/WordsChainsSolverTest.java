e3fb92460a322163b43329928e5bb7aebedefdae
sergii.tsyban <sergii.tsyban@gmail.com>
2017-08-27 00:33:14 +0200

    remove redundant words array from the test

--- src/test/java/words/solver/WordsChainsSolverTest.java
+++ src/test/java/words/solver/WordsChainsSolverTest.java
@@ -6,47 +6,52 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class WordsChainsSolverTest {
 
-    private String[] words = {
+    private Set<String> dictionary = Stream.of(
             "lead", "cog", "soon", "at", "goad", "rat", "rag", "dag",
             "gold", "dog", "query", "ruby", "rubs", "dog", "load",
-            "robs", "rods", "rode", "code", "cat", "cot","rad"};
-    private Set<String> dictionary = Stream.of(words).collect(Collectors.toSet());
+            "robs", "rods", "rode", "code", "cat", "cot", "rad")
+            .collect(Collectors.toSet());
 
     @Test
-    public void testSolveSuccess(){
+    public void testSolveSuccess() {
         WordsChainsSolver wordsChainsSolver = new WordsChainsSolver(dictionary);
         List<String> chain = wordsChainsSolver.solve("ruby", "code").firstEntry().getValue();
         Assert.assertEquals(Arrays.asList("ruby", "rubs", "robs", "rods", "rode", "code"), chain);
     }
 
     @Test
-    public void testSolveSuccessOptimal(){
-        //short "cat", "cot", "cog", "dog"
-        //long "cat", "rat", "rad", "rag", "dag", "dog"
+    public void testSolveSuccessMultiple() {
+        //"cat", "cot", "cog", "dog"
+        //"cat", "rat", "rag", "dag", "dog"
+        //"cat", "rat", "rad", "rag", "dag", "dog"
         WordsChainsSolver wordsChainsSolver = new WordsChainsSolver(dictionary);
-        List<String> chain = wordsChainsSolver.solve("cat", "dog").firstEntry().getValue();
-        Assert.assertEquals(Arrays.asList("cat", "cot", "cog", "dog"), chain);
+        TreeMap<Integer, List<String>> chains = wordsChainsSolver.solve("cat", "dog");
+        Assert.assertEquals(3, chains.size());
+        Assert.assertEquals(Arrays.asList("cat", "cot", "cog", "dog"), chains.pollFirstEntry().getValue());
+        Assert.assertEquals(Arrays.asList("cat", "rat", "rag", "dag", "dog"), chains.pollFirstEntry().getValue());
+        Assert.assertEquals(Arrays.asList("cat", "rat", "rad", "rag", "dag", "dog"), chains.pollFirstEntry().getValue());
     }
 
     @Test
-    public void testSolveFailure(){
+    public void testSolveFailure() {
         WordsChainsSolver wordsChainsSolver = new WordsChainsSolver(dictionary);
         Assert.assertTrue(wordsChainsSolver.solve("ruby", "soon").isEmpty());
     }
 
     @Test
-    public void testSolveSameWords(){
+    public void testSolveSameWords() {
         WordsChainsSolver wordsChainsSolver = new WordsChainsSolver(dictionary);
         Assert.assertTrue(wordsChainsSolver.solve("ruby", "ruby").isEmpty());
     }
 
     @Test
-    public void testSolveNonExistingWords(){
+    public void testSolveNonExistingWords() {
         WordsChainsSolver wordsChainsSolver = new WordsChainsSolver(dictionary);
         Assert.assertTrue(wordsChainsSolver.solve("robe", "cute").isEmpty());
     }
